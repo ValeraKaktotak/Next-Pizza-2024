ef6111118fb527d618283ce2509f8f9d8681973d
Valera <valerakaktotak@gmail.com>
2024-07-28 01:14:01 +0300

    done header main design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
 	return (
-		<h1>
-			Home
-			<Button variant={'outline'}>Click</Button>
-		</h1>
+		<>
+			<h1>Home</h1>
+		</>
 	)
 }
