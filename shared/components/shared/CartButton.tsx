60b392719d7451259442dd750754c5d3e534deb9
Valera <valerakaktotak@gmail.com>
2024-08-12 10:24:35 +0300

    fixed hydration warning about button in button

--- shared/components/shared/CartButton.tsx
+++ shared/components/shared/CartButton.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 import { cn } from '@/shared/lib/utils'
 
 //Components
-import { Button } from '@/shared/components/ui'
 import { CartDrawer } from './CartDrawer'
 
 interface Props {
@@ -16,7 +15,13 @@ interface Props {
 export const CartButton: React.FC<Props> = ({ className }) => {
   return (
     <CartDrawer>
-      <Button className={cn(className, 'group relative flex items-center')}>
+      <div
+        className={cn(
+          className,
+          'group relative flex items-center',
+          'inline-flex h-10 items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 active:translate-y-[1px] disabled:pointer-events-none disabled:bg-gray-500 disabled:opacity-50'
+        )}
+      >
         <b>520 ₽</b>
         <span className='mx-3 h-full w-[1px] bg-white/30' />
         <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
@@ -27,7 +32,7 @@ export const CartButton: React.FC<Props> = ({ className }) => {
           size={20}
           className='absolute right-5 -translate-x-2 opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100'
         />
-      </Button>
+      </div>
     </CartDrawer>
   )
 }
