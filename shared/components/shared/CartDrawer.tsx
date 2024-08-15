681536267f902745845a896d8e81769671cfdb0d
Valera <valerakaktotak@gmail.com>
2024-08-15 20:02:56 +0300

    added empty design for empty cart

--- shared/components/shared/CartDrawer.tsx
+++ shared/components/shared/CartDrawer.tsx
@@ -1,27 +1,31 @@
 'use client'
-import { ArrowRight } from 'lucide-react'
+import { ArrowLeft, ArrowRight } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import React, { FC, useEffect } from 'react'
 
-//Utils
+//Libs
+import { getCartItemDetails } from '@/shared/lib/getCartItemDetails'
 import { cn } from '@/shared/lib/utils'
 
 //Store
 import { useCartStore } from '@/shared/store/cart'
 
+//Types
+import type { PizzaSize, PizzaType } from '@/shared/constants/pizza'
+
 //Components
-import { CartDrawerItem } from '@/shared/components/shared/CartDrawerItem'
+import { CartDrawerItem, Title } from '@/shared/components/shared'
 import { Button } from '@/shared/components/ui'
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger
 } from '@/shared/components/ui/sheet'
-import { PizzaSize, PizzaType } from '@/shared/constants/pizza'
-import { getCartItemDetails } from '@/shared/lib/getCartItemDetails'
 
 interface Props {
   className?: string
@@ -63,58 +67,98 @@ export const CartDrawer: FC<React.PropsWithChildren<Props>> = ({
       <Sheet>
         <SheetTrigger>{children}</SheetTrigger>
         <SheetContent className='flex flex-col justify-between bg-[#f4f1ee] pb-0'>
-          <SheetHeader>
-            <SheetTitle>
-              В корзине <span className='font-bold'>{items.length} товара</span>
-            </SheetTitle>
-          </SheetHeader>
+          <div
+            className={cn(
+              'flex h-full flex-col',
+              !totalAmount && 'justify-center'
+            )}
+          >
+            {totalAmount > 0 && (
+              <SheetHeader>
+                <SheetTitle>
+                  В корзине{' '}
+                  <span className='font-bold'>{items.length} товара</span>
+                </SheetTitle>
+              </SheetHeader>
+            )}
 
-          <div className='-mx-6 mt-5 flex-1 overflow-auto'>
-            {items.map((item) => (
-              <CartDrawerItem
-                key={item.id}
-                id={item.id}
-                imageUrl={item.imageUrl}
-                disabled={item.disabled}
-                details={
-                  item.pizzaType && item.pizzaSize
-                    ? getCartItemDetails({
-                        ingredients: item.ingredients,
-                        pizzaType: item.pizzaType as PizzaType,
-                        pizzaSize: item.pizzaSize as PizzaSize
-                      })
-                    : ''
-                }
-                name={item.name}
-                price={item.price}
-                quantity={item.quantity}
-                onClickCountButton={(type) => {
-                  onClickCountButton(item.id, item.quantity, type)
-                }}
-                onClickRemove={() => removeCartItem(item.id)}
-              />
-            ))}
-          </div>
+            {!totalAmount && (
+              <div className='mx-auto flex w-72 flex-col items-center justify-center'>
+                <Image
+                  src='/images/tools/empty-box.png'
+                  alt='Empty cart'
+                  width={120}
+                  height={120}
+                />
+                <Title
+                  size='sm'
+                  text='Корзина пустая'
+                  className='my-2 text-center font-bold'
+                />
+                <p className='mb-5 text-center text-neutral-500'>
+                  Добавьте хотя бы одну пиццу, чтобы совершить заказ
+                </p>
 
-          <SheetFooter className='-mx-6 bg-white p-8'>
-            <div className='w-full'>
-              <div className='mb-4 flex'>
-                <span className='flex flex-1 text-lg text-neutral-500'>
-                  Итого
-                  <div className='relative -top-1 mx-2 flex-1 border-b border-dashed border-b-neutral-200' />
-                </span>
-
-                <span className='text-lg font-bold'>{totalAmount} ₽</span>
+                <SheetClose>
+                  <Button className='h-12 w-56 text-base' size='lg'>
+                    <ArrowLeft className='mr-2 w-5' />
+                    Вернуться назад
+                  </Button>
+                </SheetClose>
               </div>
+            )}
 
-              <Link href='/checkout'>
-                <Button type='submit' className='h-12 w-full text-base'>
-                  Оформить заказ
-                  <ArrowRight className='ml-2 w-5' />
-                </Button>
-              </Link>
-            </div>
-          </SheetFooter>
+            {totalAmount > 0 && (
+              <>
+                <div className='-mx-6 mt-5 flex-1 overflow-auto'>
+                  {items.map((item) => (
+                    <CartDrawerItem
+                      key={item.id}
+                      id={item.id}
+                      imageUrl={item.imageUrl}
+                      disabled={item.disabled}
+                      details={
+                        item.pizzaType && item.pizzaSize
+                          ? getCartItemDetails({
+                              ingredients: item.ingredients,
+                              pizzaType: item.pizzaType as PizzaType,
+                              pizzaSize: item.pizzaSize as PizzaSize
+                            })
+                          : ''
+                      }
+                      name={item.name}
+                      price={item.price}
+                      quantity={item.quantity}
+                      onClickCountButton={(type) => {
+                        onClickCountButton(item.id, item.quantity, type)
+                      }}
+                      onClickRemove={() => removeCartItem(item.id)}
+                    />
+                  ))}
+                </div>
+
+                <SheetFooter className='-mx-6 bg-white p-8'>
+                  <div className='w-full'>
+                    <div className='mb-4 flex'>
+                      <span className='flex flex-1 text-lg text-neutral-500'>
+                        Итого
+                        <div className='relative -top-1 mx-2 flex-1 border-b border-dashed border-b-neutral-200' />
+                      </span>
+
+                      <span className='text-lg font-bold'>{totalAmount} ₽</span>
+                    </div>
+
+                    <Link href='/checkout'>
+                      <Button type='submit' className='h-12 w-full text-base'>
+                        Оформить заказ
+                        <ArrowRight className='ml-2 w-5' />
+                      </Button>
+                    </Link>
+                  </div>
+                </SheetFooter>
+              </>
+            )}
+          </div>
         </SheetContent>
       </Sheet>
     </div>
