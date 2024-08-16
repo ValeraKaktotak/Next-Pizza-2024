9d452e17f862911957484f544606a845cd99ed42
Valera <valerakaktotak@gmail.com>
2024-08-16 12:23:17 +0300

    added WhiteBlock component

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,3 +1,14 @@
+import { Container, Title, WhiteBlock } from '@/shared/components/shared'
+
 export default function CheckoutPage() {
-  return <div>Checkout</div>
+  return (
+    <Container>
+      <Title
+        text='Оформление заказа'
+        className='mb-8 mt-10 text-[36px] font-extrabold'
+      />
+      <WhiteBlock title='1. Корзина'>123</WhiteBlock>
+      <WhiteBlock title='2. Персональные данные'>123</WhiteBlock>
+    </Container>
+  )
 }
