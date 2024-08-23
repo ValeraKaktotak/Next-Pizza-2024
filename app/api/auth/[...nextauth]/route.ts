b797f63563de03c5f222d159a04738f82af0a781
Valera <valerakaktotak@gmail.com>
2024-08-23 16:35:03 +0300

    done creating user in DB with use gitHub authentification

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from '@/prisma/prisma-client'
 import { UserRole } from '@prisma/client'
-import { compare } from 'bcrypt'
+import { compare, hashSync } from 'bcrypt'
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GitHubProvider from 'next-auth/providers/github'
@@ -70,6 +70,59 @@ export const authOptions = {
     strategy: 'jwt'
   },
   callbacks: {
+    async signIn({ user, account }) {
+      try {
+        if (account?.provider === 'credentials') {
+          return true
+        }
+
+        if (!user.email) {
+          return false
+        }
+
+        const findUser = await prisma.user.findFirst({
+          where: {
+            OR: [
+              {
+                provider: account?.provider,
+                providerId: account?.providerAccountId
+              },
+              { email: user.email }
+            ]
+          }
+        })
+
+        if (findUser) {
+          await prisma.user.update({
+            where: {
+              id: findUser.id
+            },
+            data: {
+              provider: account?.provider,
+              providerId: account?.providerAccountId
+            }
+          })
+
+          return true
+        }
+
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            fullName: user.name || 'User #' + user.id,
+            password: hashSync(user.id.toString(), 10),
+            verified: new Date(),
+            provider: account?.provider,
+            providerId: account?.providerAccountId
+          }
+        })
+
+        return true
+      } catch (error) {
+        console.error('Error [SIGNIN]', error)
+        return false
+      }
+    },
     async jwt({ token }) {
       const findUser = await prisma.user.findFirst({
         where: {
