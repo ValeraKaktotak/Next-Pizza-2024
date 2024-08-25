5f511c0a1ec7f0806648f349c26d65b8f8be4baa
Valera <95238664+ValeraKaktotak@users.noreply.github.com>
2024-08-26 00:37:54 +0300

    Update VerificationUserTemplate.tsx
    change URL address

--- shared/components/shared/email-templates/VerificationUserTemplate.tsx
+++ shared/components/shared/email-templates/VerificationUserTemplate.tsx
@@ -11,7 +11,7 @@ export const VerificationUserTemplate: React.FC<Props> = ({ code }) => (
     </p>
 
     <p>
-      <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>
+      <a href={`https://next-pizza-2024-virid.vercel.app/api/auth/verify?code=${code}`}>
         Подтвердить регистрацию
       </a>
     </p>
